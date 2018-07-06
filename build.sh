08bddcfc53b8450514402a29e8cba172c1853d7d
Leonti <prishelec@gmail.com>
2018-07-06 21:37:12 +1000

    Automate the build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 sudo docker build -t leonti/vault:$version .
 sudo docker push leonti/vault
-git tag -a v$version -m 'new version $version'
 
-echo "Released version $version"
-
+echo $version" is built"
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-version=$1
-docker stop vault
-docker rm vault
-sudo docker run -d -v /data:/data -e SETTINGS=/data/settings.cfg --name vault leonti/vault
